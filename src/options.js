0696c28edb7434fcb3f5fff892d5c4d02e2cda7b
kapsi <kapsi@o2.pl>
2015-05-04 20:30:46 +0200

    Add tests

---
+++ src/options.js
@@ -0,0 +1,90 @@
+function getMangaDirectory(mangaDirectoryURL, callback, errorCallback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', mangaDirectoryURL);
+    xhr.responseType = "document";
+    xhr.onload = function () {
+        if (this.status === 200) {
+            var page = this.response;
+            callback(page);
+        } else errorCallback(this);
+    };
+    xhr.onerror = errorCallback;
+    xhr.send();
+}
+
+function showError() {
+    document.write('<h1>Error while downloading data from mangastream. Check your connection or try again later.</h1>');
+}
+
+function onMangaSelected() {
+    var selectedManga = [];
+    var selectedCheckboxes = document.querySelectorAll('input:checked');
+    for (var i = 0; i < selectedCheckboxes.length; i++) {
+        selectedManga.push(selectedCheckboxes[i].value);
+    }
+    localStorage.setItem('selectedManga', JSON.stringify(selectedManga));
+    chrome.runtime.sendMessage({type: 'selectedManga', newVal: selectedManga});
+}
+
+function createMangaList(mangaDirectoryPage) {
+    var list = document.querySelector('#list');
+    var rows = mangaDirectoryPage.querySelectorAll('tr');
+    for (var i = 1; i < rows.length; i++) { //row 0 is table header
+        var title = rows[i].querySelector('td').textContent;
+        var checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.value = title;
+        checkbox.id = title;
+        checkbox.onchange = onMangaSelected;
+        if (getSelectedManga().indexOf(title) !== -1) {
+            checkbox.checked = true;
+        }
+        var label = document.createElement('label');
+        label.textContent = title;
+        label.insertBefore(checkbox, label.childNodes[0]);
+        label.appendChild(document.createElement('br'));
+        list.appendChild(label);
+    }
+}
+
+function initMangaFilter() {
+    document.querySelector('#filter').onkeyup = function filterCheckboxes(event) {
+        var filterText = this.value.toLowerCase();
+        var labels = document.querySelectorAll('#list label');
+        for (var i = 0; i < labels.length; i++) {
+            var label = labels[i];
+            if (label.textContent.toLowerCase().indexOf(filterText) === -1) label.style.display = 'none';
+            else label.style.display = 'inline';
+        }
+    }
+}
+
+function initCheckPeriodOption() {
+    var cpEl = document.querySelector('#check_period');
+    cpEl.value = getCheckPeriod();
+    cpEl.onchange = function () {
+        chrome.runtime.sendMessage({type: 'checkPeriod', newVal: this.value});
+        localStorage.setItem('checkPeriod', this.value);
+    };
+    cpEl.onchange();
+}
+
+function getSelectedManga() {
+    var selectedManga = localStorage.getItem('selectedManga');
+    if (selectedManga) selectedManga = JSON.parse(selectedManga);
+    else selectedManga = [];
+    return selectedManga;
+}
+
+function getCheckPeriod() {
+    var checkPeriod = localStorage.getItem('checkPeriod');
+    if (checkPeriod) checkPeriod = parseInt(checkPeriod, 10);
+    else checkPeriod = 15;
+    return checkPeriod;
+}
+
+function init() {
+    initMangaFilter();
+    initCheckPeriodOption();
+    getMangaDirectory('http://mangastream.com/manga', createMangaList, showError);
+}
---
+++ test/options.test.js
@@ -0,0 +1,58 @@
+describe("options module", function () {
+    var mangaDirectoryPage, mangaDirectoryPageString, server;
+    beforeAll(function () {
+        fixture.setBase('test');
+        mangaDirectoryPageString = window.__html__['test/manga_directory.html'];
+        var doctype = document.implementation.createDocumentType('html', '', '');
+        mangaDirectoryPage = document.implementation.createDocument('', 'html', doctype);
+        mangaDirectoryPage.documentElement.innerHTML = mangaDirectoryPageString;
+    });
+    beforeEach(function () {
+        fixture.load('options.fixture.html');
+    });
+    describe('getMangaDirectory', function () {
+        var callback, errorCallback, url;
+
+        beforeEach(function () {
+            callback = jasmine.createSpy('callback');
+            errorCallback = jasmine.createSpy('error callback');
+            jasmine.Ajax.install();
+        });
+        afterEach(function () {
+            jasmine.Ajax.uninstall();
+        });
+        it('requests the manga directory page and if the request is successful, passes it to the callback', function () {
+            url = 'fakeUrl';
+            getMangaDirectory(url, callback, errorCallback);
+            expect(jasmine.Ajax.requests.mostRecent().url).toBe(url);
+            expect(callback).not.toHaveBeenCalled();
+            expect(errorCallback).not.toHaveBeenCalled();
+
+            jasmine.Ajax.requests.mostRecent().respondWith({
+                "status": 200,
+                "contentType": 'document/html',
+                "responseText": mangaDirectoryPageString
+            });
+            expect(callback).toHaveBeenCalledWith(mangaDirectoryPageString);
+            expect(errorCallback).not.toHaveBeenCalled();
+        });
+        it('calls error callback if request fails', function () {
+            url = 'fakeErrorUrl';
+            getMangaDirectory(url, callback, errorCallback);
+            jasmine.Ajax.requests.mostRecent().responseError();
+            expect(errorCallback).toHaveBeenCalled();
+        });
+        it('calls error callback if server returns 404', function () {
+            url = 'fakeErrorUrl';
+            getMangaDirectory(url, callback, errorCallback);
+            jasmine.Ajax.requests.mostRecent().respondWith({
+                "status": 404
+            });
+            expect(errorCallback).toHaveBeenCalled();
+        });
+    });
+    it('creates manga list from a manga directory page', function () {
+        createMangaList(mangaDirectoryPage);
+        expect(document.querySelectorAll('label').length).toEqual(47);
+    });
+});
